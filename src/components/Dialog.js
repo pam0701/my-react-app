bc9019d8d5a898fa95b85a2b4a85c96b5073425e
pam0701 <psb04027@naver.com>
2022-10-05 14:54:05 +0900

    Dialog.js, WelcomeDialog.js 구현2

---
+++ src/components/Dialog.js
@@ -0,0 +1,8 @@
+export default function Dialog(props) {
+  return (
+    <div style={{ backgroundColor: props.color }}>
+      <h1>{props.title}</h1>
+      <p>{props.message}</p>
+    </div>
+  );
+}
---
+++ src/components/WelcomeDialog.js
@@ -0,0 +1,10 @@
+import Dialog from "./Dialog";
+export default function WelcomeDialog() {
+  return (
+    <Dialog
+      color="orange"
+      title="Welcome to summoner's lift"
+      message="소환사의 협곡에 오신 걸 환영합니다."
+    />
+  );
+}
